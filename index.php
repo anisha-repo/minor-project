b7859910bfbfc917b0e783c582e47e2095a37189
Ayush <ayush115@outlook.com>
2024-09-21 00:29:00 +0530

    inline changes

--- index.php
+++ index.php
@@ -35,34 +35,34 @@
                            </div>
                         </div>
                     </div><!--col-md-3-end-->
-                     
+
                     <div class="col-md-6 pro-details-hide"><!--col-md-6-start-->
                        <div class="box">
                            <h1>Sneaker</h1>
                            <p>product Description</p>
-                          
+
                            <p class="price">rs.1500&nbsp&nbsp&nbsp&nbsp</p>
-                           
+
                            <span class="text-primary">40% Off</span>
                         </div>
                         <div class="row">
-                           
+
                             <p class="text-control-buttons qt-sneaker text-center">
                                 <label class="control-label size">
                                     <legend class="varient-label"><b>Size(UK)&nbsp&nbsp</b></legend>
                                     <span class="size-buttons"> <!--size-button start-->
-                                       <button type="button" class="buttons">6</button> 
-                                       <button type="button" class="buttons">7</button> 
-                                       <button type="button" class="buttons">8</button> 
-                                       <button type="button" class="buttons">9</button> 
+                                       <button type="button" class="buttons">6</button>
+                                       <button type="button" class="buttons">7</button>
+                                       <button type="button" class="buttons">8</button>
+                                       <button type="button" class="buttons">9</button>
                                     </span><!--size-button end-->
-                                </label>    
-                               
+                                </label>
+
                                 <label class="control-label">
                                       <button class="col-md-9 btn btn-info text-center" type="submit" id="cart">
                                         <i class="fa fa-shopping-cart">Add to cart</i>
                                       </button>
-                                </label> 
+                                </label>
                             </p>
                         </div>
                         <div class="Acord"> <!--Accordian start-->
@@ -87,14 +87,14 @@
                        </div><!--col-md-6-end-->
                     </div><!--row end-->
 
-                    <div class="row"><!--row start-->
-                        <div class="center-responsive"><!--col-md-3-start-->
+                    <!--<div class="row">
+                        <div class="center-responsive">
                            <div class="products">
                               <a href="index.php"><img src="styles/luis-felipe-lins-LG88A2XgIXY-unsplash (1).jpg" alt=""/></a>
                               <div class="you-may-like-product">
                                 <a href=index.php><h5 class="product-name">sneakers</h5></a>
                                 <p class="product-price"><small class="text-danger"><s>Rs. 1500</s></small></p>&nbsp&nbsp
-                                <span class="text-success">Rs.1000</span> 
+                                <span class="text-success">Rs.1000</span>
                                </div>
                             <div>
 
@@ -103,17 +103,43 @@
                               <div class="you-may-like-product">
                                 <a href=index.php><h5 class="product-name">sneakers</h5></a>
                                 <p class="product-price"><small class="text-danger"><s>Rs. 1500</s></small></p>&nbsp&nbsp
-                                <span class="text-success">Rs.1000</span> 
+                                <span class="text-success">Rs.1000</span>
                               </div>
                             <div>
-                        </div><!--col-md-3-end-->
-                    </div><!--row end-->
+                        </div>
+                    </div>-->
+                    <div class="flex-container">
+                      <div class="products">
+                        <a href="index.php"><img src="styles/luis-felipe-lins-LG88A2XgIXY-unsplash (1).jpg" alt=""/></a>
+                              <div class="you-may-like-product">
+                                <a href=index.php><h5 class="product-name">sneakers</h5></a>
+                                <p class="product-price"><small class="text-danger"><s>Rs. 1500</s></small></p>&nbsp&nbsp
+                                <span class="text-success">Rs.1000</span>
+                               </div>
+                      </div>
+                      <div class="products">
+                        <a href="index.php"><img src="styles/luis-felipe-lins-LG88A2XgIXY-unsplash (1).jpg" alt=""/></a>
+                              <div class="you-may-like-product">
+                                <a href=index.php><h5 class="product-name">sneakers</h5></a>
+                                <p class="product-price"><small class="text-danger"><s>Rs. 1500</s></small></p>&nbsp&nbsp
+                                <span class="text-success">Rs.1000</span>
+                               </div>
+                      </div>
+                      <div class="products">
+                        <a href="index.php"><img src="styles/luis-felipe-lins-LG88A2XgIXY-unsplash (1).jpg" alt=""/></a>
+                              <div class="you-may-like-product">
+                                <a href=index.php><h5 class="product-name">sneakers</h5></a>
+                                <p class="product-price"><small class="text-danger"><s>Rs. 1500</s></small></p>&nbsp&nbsp
+                                <span class="text-success">Rs.1000</span>
+                               </div>
+                      </div>
+                    </div>
 
-                    
+
                 </div><!--container end-->
-               
-                
-                      
+
+
+
                     </div>
                 </div>
             </div><!--container end 2-->
